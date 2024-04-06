fab976f7f1f78b978034cc3bdda2e230fcaaf875
unknown <abisovfarid14@gmail.com>
2024-04-06 21:57:20 +0400

    axios

--- reduxlesson/src/App.js
+++ reduxlesson/src/App.js
@@ -1,15 +1,45 @@
-import './App.css';
-import CounterBtn from './components/CounterBtn';
-import CounterValue from './components/CounterValue';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const App = () => {
+  const [todos, setTodos] = useState([]); 
 
-function App() {
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos/"
+        );
+        setTodos(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <CounterValue></CounterValue>
-      <CounterBtn></CounterBtn>
+    <div>
+
+      <div className="todo-wrapper">
+
+        {todos.length > 0 &&
+          todos.map((todo) => (
+            <div key={todo.id} className="todo">
+              <h3 className="todo-title">{todo.title}</h3>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                className="todo-completed"
+              />
+            </div>
+          ))}
+
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
